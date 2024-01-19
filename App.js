98c684a95b678818873e58d7e1aea69a88a6353a
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2024-01-19 13:25:10 -0300

    Se a creado un prototipo funcinal que comprueba los cupones creados desde la base de datos

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import ConsultaDB from './components/ConsultaDB';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [message, setMessage] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleConsultar = () => {
+    if (inputValue.trim() === "") {
+      setMessage("Por favor ingresa un numero de cupon");
+      return;
+    }
+    const codigoCupon = inputValue.replace("-", "");
+    setInputValue(codigoCupon)
+    setModalVisible(true);
+  };
+
+  const formatInput = (text) => {
+    // Elimina caracteres no numéricos
+    const formattedText = text.replace(/[^\d]/g, '');
+
+    // Aplica el formato según la longitud
+    if (formattedText.length === 6) {
+      setInputValue(`${formattedText.slice(0, 1)}-${formattedText.slice(1)}`);
+    } else if (formattedText.length === 7) {
+      setInputValue(`${formattedText.slice(0, 2)}-${formattedText.slice(2)}`);
+    } else if (formattedText.length === 8) {
+      setInputValue(`${formattedText.slice(0, 3)}-${formattedText.slice(3)}`);
+    } else if (formattedText.length >= 9) {
+      setInputValue(`${formattedText.slice(0, 4)}-${formattedText.slice(4)}`);
+    } else {
+      setInputValue(formattedText);
+    }
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <View style={styles.titleContain}>
+          <Text style={styles.title}>TucuPon</Text>
+        </View>
+        <View style={styles.descriptionContain}>
+          <Text style={styles.description}>Coloca el codigo del cupon y verifica su informacion</Text>
+          {message && <Text style={styles.message}>Por favor ingresa un numero de cupon</Text>}
+        </View>
+        <View style={styles.containerInput}>
+          <TextInput
+            placeholder="XX-XXXXX"
+            value={inputValue}
+            onChangeText={formatInput}
+            keyboardType="numeric"
+            maxLength={9}
+            style={styles.inputCupon}
+          />
+        </View>
+        <TouchableOpacity style={[styles.buttonVerificar, styles.shadow]} title="Verificar cupon" onPress={handleConsultar}>
+          <Text style={styles.appButtonText}>Verificar cupon</Text>
+        </TouchableOpacity>
+      </View>
+      <ConsultaDB inputValue={inputValue} closeModal={closeModal} visible={modalVisible} />
     </View>
   );
 }
@@ -17,4 +75,64 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonVerificar: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    backgroundColor: '#97AF41',
+    borderRadius: 20,
+  },
+  appButtonText: {
+    textAlign: 'center',
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  containerInput: {
+    borderWidth: 4,
+    borderColor: "#5B6B22",
+    borderStyle: "dashed",
+    borderRadius: 20,
+    padding: 13,
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  inputCupon: {
+    fontSize: 22,
+    textAlign: 'center',
+    width: "100%"
+  },
+  titleContain: {
+    alignItems: "center",
+  },
+  descriptionContain: {
+    alignItems: "center",
+    marginBottom: 15
+  },
+  title: {
+    fontSize: 45,
+    color: "#5B6B22",
+    fontWeight: "bold"
+  },
+  message: {
+    fontSize: 15,
+    color: "#5B6B22",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  description: {
+    fontSize: 17,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  }
 });
---
+++ components/ConsultaDB.jsx
@@ -0,0 +1,251 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View, Modal, TouchableOpacity, StyleSheet, Image, ActivityIndicator } from 'react-native';
+import axios from 'axios';
+
+const ConsultaDB = ({ inputValue, closeModal, visible }) => {
+    const [data, setData] = useState(null);
+    const [load, setLoad] = useState(false);
+    const [error, setError] = useState(false);
+    const [msgError, setMsgError] = useState("");
+
+    const fetchData = async () => {
+        setLoad(true);
+        setError(false);
+        try {
+            const response = await axios.get(`https://visitatucuman.ar/api/cupones?codigo=${inputValue}`);
+            const responseJson = response.data;
+            if (responseJson.status === 200) {
+                setData(responseJson.result);
+            } else {
+                setError(true);
+                setMsgError("El cupon no fue encontrado");
+            }
+            setLoad(false);
+        } catch (error) {
+            setError(true);
+            setMsgError("Hubo un error al intentar consultar el cupon, inténtelo más tarde");
+            console.error('Error al consultar la base de datos', error);
+            setLoad(false);
+        }
+    };
+
+    /* const burnHandler = async () => {
+        try {
+            const response = await axios.post(
+                'http://10.15.15.151/touchvanilla/api/cupones',
+                {
+                    codigo: '1149020'
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                }
+            );
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error al consultar la base de datos', error);
+        }
+    } */
+
+    useEffect(() => {
+        if (visible && inputValue.trim() !== "") {
+            fetchData();
+        }
+    }, [visible])
+
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={visible}
+            onRequestClose={closeModal}
+        >
+            <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                    {load ? (
+                        <ActivityIndicator style={styles.loading} />
+                    ) : error ? (
+                        <Text style={styles.textError}>{msgError}</Text>
+                    ) : data ? (
+                        <>
+                            <View style={styles.headContent}>
+                                <View style={styles.imgCuponContain}>
+                                    <Image
+                                        style={styles.imgCupon}
+                                        source={{
+                                            uri: 'https://www.tucumanturismo.gob.ar/carga/cupon/' + data.imagen,
+                                        }}
+                                        resizeMode="contain"
+                                    />
+                                </View>
+                                <View style={styles.infoHeadContain}>
+                                    <Text style={styles.textTitle}>{data.nombre}</Text>
+                                    <Text style={styles.textDescuento}>{data.promocion}</Text>
+                                    <Text style={styles.textDesc}>{data.descripcion}</Text>
+                                </View>
+                            </View>
+
+                            <View style={styles.hr}></View>
+
+                            <View style={styles.cuponContain}>
+                                <Text style={styles.textCupon}>{data.id}-{data.clave}</Text>
+                            </View>
+                            <View style={styles.vencContain}>
+                                <Text style={styles.textVencTitle}>Vencimiento:</Text>
+                                <Text style={styles.textVenc}>{data.fechaVenc}</Text>
+                            </View>
+
+                            <View style={styles.hr}></View>
+
+                            <View style={styles.personaContain}>
+                                <View style={styles.infoPersonalContain}>
+                                    <Text style={styles.textPersonal}>Nombre:</Text>
+                                    <Text style={styles.textPersonal}>{data.nombrePersona} {data.apellidoPersona}</Text>
+                                </View>
+                                <View style={styles.infoPersonalContain}>
+                                    <Text style={styles.textPersonal}>DNI:</Text>
+                                    <Text style={styles.textPersonal}>{data.dniPersona}</Text>
+                                </View>
+                                <View style={styles.infoPersonalContain}>
+                                    <Text style={styles.textPersonal}>Telefono:</Text>
+                                    <Text style={styles.textPersonal}>{data.telefonoPersona}</Text>
+                                </View>
+                            </View>
+                        </>
+                    ) : null
+                    }
+                    <View style={styles.containButtons}>
+                        <TouchableOpacity style={[styles.closeButton, styles.shadow]} title="Cerrar" onPress={closeModal}>
+                            <Text style={styles.appButtonText}>Cerrar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+    },
+    headContent: {
+        width: '100%',
+        flexDirection: 'row',
+    },
+    cuponContain: {
+        backgroundColor: "#8EA831",
+        width: "100%",
+        alignItems: 'center',
+        borderRadius: 8,
+        marginBottom: 5
+    },
+    vencContain: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+    },
+    infoPersonalContain: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    personaContain: {
+        width: "100%",
+        marginBottom: 10
+    },
+    imgCuponContain: {
+        flex: 3,
+        height: 150,
+    },
+    infoHeadContain: {
+        flex: 4,
+        paddingLeft: 10
+    },
+    imgCupon: {
+        width: "100%",
+        height: "100%",
+    },
+    textVencTitle: {
+        fontSize: 20,
+        textAlign: "center",
+        fontWeight: "bold",
+    },
+    textVenc: {
+        fontSize: 23,
+        textAlign: "center"
+    },
+    textTitle: {
+        fontSize: 23,
+        fontWeight: 'bold',
+    },
+    textError: {
+        fontSize: 23,
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+    textPersonal: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5
+    },
+    textDescuento: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#4F6E00',
+    },
+    textDesc: {
+        fontSize: 17,
+    },
+    textCupon: {
+        fontSize: 35,
+        color: "white"
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        paddingHorizontal: 20
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: "100%",
+    },
+    containButtons: {
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-around",
+    },
+    closeButton: {
+        paddingVertical: 12,
+        backgroundColor: '#5B6B22',
+        borderRadius: 8,
+        width: "100%"
+    },
+    appButtonText: {
+        textAlign: 'center',
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+    hr: {
+        borderWidth: 0.2,
+        borderColor: 'gray',
+        margin: 10,
+        width: "100%",
+    },
+    loading: {
+        marginBottom: 10,
+        width: 50
+    },
+});
+
+export default ConsultaDB;
